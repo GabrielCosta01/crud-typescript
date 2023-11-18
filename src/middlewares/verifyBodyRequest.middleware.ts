d1988beec4e53677eb89afbdacc7ad5fc0ea1771
GabrielCosta01 <gabrielcorreacostagt@gmail.com>
2023-11-17 22:54:38 -0300

    created verify schema middleware for all routes and implementation this middleware in creation user

---
+++ src/middlewares/verifyBodyRequest.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { AnySchema } from "yup";
+import { AppError } from "../error";
+
+export const verifyBodyMiddleware = (schema:AnySchema) => async (req:Request, res:Response, next:NextFunction) => {
+  const validateBody = await schema.validate(req.body, {abortEarly:false, stripUnknown:true})
+  .catch((err) => {
+    return res.status(400).json({'message':err.errors})
+  });
+
+  req.body = validateBody;
+
+  return next();
+};
